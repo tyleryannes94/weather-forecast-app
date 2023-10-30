1eef7e8893ceb8115bcf6e134f6979bcdbd2ac5a
tyleryannes94 <tyler.yannes@gmail.com>
2023-10-30 06:45:03 -0500

    Added an image url const to my forecast functions and added the image icon fetched from the API.

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,29 @@
 //Pseudo-code outline:
-// Create var for API Key
-// Create fetch function using API key 
-// Filter the fetch function for Date, image(?), temp, wind, and humidity; limit to 5 day 
-// Create function that returns fetch for city input
-//  - Use this data to display today's forecast in a specific class
-//  - Use next 5 days data to push into their own block
-//  - Store the city input in localStorage to and append class under a search history section
 // (If time allows) add a "home city star" button that appends the forecast at the bottom for a quick comparison to 
 // what city you're searching for
 
+// Create var for API Key
 const APIKey = "ada5c877c3f1ff4c35d2f2a88ca7ed5b";
 
+// Create fetch function using API key 
 async function cityWeatherForecast(city = "Austin") {
     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
 
     try {
         const response = await fetch(forecastURL);
-
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        
         const data = await response.json();
-
         // Display today's forecast
-        displayTodayForecast(data.list[0]);
-
+        displayTodayForecast(data.list[0], city);
         // Clear previous daily forecasts
         const dailyForecastsEl = document.getElementById('daily-forecasts');
         dailyForecastsEl.innerHTML = "";
-
         // Display forecast for the next 5 days
         for (let i = 1; i <= 5; ++i) {
             displayDailyForecast(data.list[i * 8 - 1]);
         }
-
         // Store the city in local storage
         localStorage.setItem('lastSearchedCity', city);
     } catch (error) {
@@ -42,41 +31,43 @@ async function cityWeatherForecast(city = "Austin") {
     }
 }
 
-function displayTodayForecast(forecast) {
+function displayTodayForecast(forecast, cityName) {
     const todayForecastEl = document.getElementById('today-forecast');
-    
+    const iconURL = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+
     const content = `
-        <h2>Today</h2>
+        <h2>Today in ${cityName}</h2>
+        <img src="${iconURL}" alt="Weather icon">
         <p>Temperature: ${forecast.main.temp}°F</p>
         <p>Weather: ${forecast.weather[0].description}</p>
-        <p>Humidity: ${forecast.main.temp}%</p>
+        <p>Humidity: ${forecast.main.humidity}%</p>
         <p>Wind Speed: ${forecast.wind.speed} mph</p>
-
     `;
-
     todayForecastEl.innerHTML = content;
 }
 
+// Filter the fetch function for Date, image(?), temp, wind, and humidity; limit to 5 day 
 function displayDailyForecast(forecast) {
     const dailyForecastsEl = document.getElementById('daily-forecasts');
-    
+    const iconURL = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+
     const content = `
         <div class="daily-forecast">
             <h3>${new Date(forecast.dt * 1000).toLocaleDateString()}</h3>
+            <img src="${iconURL}" alt="Weather icon">
             <p>Temperature: ${forecast.main.temp}°F</p>
             <p>Weather: ${forecast.weather[0].description}</p>
-            <p>Humidity: ${forecast.main.temp}%</p>
+            <p>Humidity: ${forecast.main.humidity}%</p>
             <p>Wind Speed: ${forecast.wind.speed} mph</p>
         </div>
     `;
-
     dailyForecastsEl.innerHTML += content;
 }
 
+// Create function that returns fetch for city input
 function searchCity() {
     const city = document.getElementById('city-input').value;
     cityWeatherForecast(city);
 }
-
 
 cityWeatherForecast("Austin");
